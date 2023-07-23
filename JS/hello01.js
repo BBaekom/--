19426d2d9db0c75d6682f76d86ad8649d87455f1
Pig <min208406@naver.com>
2023-07-24 00:58:14 +0900

    Section14 Complete

--- JS/hello01.js
+++ JS/hello01.js
@@ -1,3 +1,20 @@
 console.log("hello world");
 console.log(3.5 * 2);
 console.log(typeof NaN);
+
+const year = 1985;
+console.log(year);
+
+var runDistance = 26.2;
+console.log(runDistance);
+
+console.log(true);
+
+let run = 0;
+console.log(run);
+
+console.log(typeof run);
+
+const isWaterWet = true;
+
+console.log(isWaterWet);
