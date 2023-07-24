6e299dcd463fa2a29c3ce2e22787e8337c5c81f1
Pig <min208406@naver.com>
2023-07-24 17:22:35 +0900

    Section16 Complete

--- JS/FirstEx/hello01.js
+++ JS/FirstEx/hello01.js
@@ -46,4 +46,9 @@ console.log(Math.ceil(33.445));
 console.log(Math.random());
 console.log(Math.floor(Math.random() * 5) + 1);
 console.log(Math.pow(2, 5));
-
+console.log(3 >= 3);
+console.log("이종민 여자친구는 김준하");
+console.log(0 !== false);
+console.log(1 == '1');
+console.log(1 + 4, "HI", true);
+console.warn("UH OH WARNING!");
