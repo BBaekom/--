2eb82044b29d00e07fd224e2b96dbd4e1484573a
Pig <min208406@naver.com>
2023-08-01 01:54:08 +0900

    함수 범위 Complete

--- JS/Section21/Scope/app.js
+++ JS/Section21/Scope/app.js
@@ -1,53 +1,54 @@
 // ==============
 // FUNCTION SCOPE
-// ==============
-let totalEggs = 0;
-function collectEggs() {
-    totalEggs = 6;
-}
-console.log(totalEggs);
-collectEggs();
-console.log(totalEggs);
+// ============== 
+// let totalEggs = 0;
+// function collectEggs() {
+//     totalEggs = 6;
+// }
+// console.log(totalEggs);
+// collectEggs();
+// console.log(totalEggs);
 
-const bird = 'Scarlet Macaw';
-function birdWatch() {
-    const bird = 'Great Blue Heron';
-    console.log(bird);
-}
-birdWatch()
+// const bird = 'Scarlet Macaw';
+// function birdWatch() {
+//     const bird = 'Great Blue Heron';
+//     console.log(bird);
+// }
+// birdWatch()
 
 // ==============
-// BLOCK SCOPE
+// BLOCK SCOPE 
 // ==============
 // let radius = 8;
 // if (radius > 0) {
 //     const PI = 3.14159;
-//     let msg = 'HIII!'
+//     let msg = 'HIII!';
 // }
 // console.log(radius);
-// console.log(msg)
+// console.log(msg);
 
 // for (let i = 0; i < 5; i++) {
-//     let msg = "ASKLDJAKLSJD";
-//     console.log(msg)
+//     let msg = 'ASKLDJAKLSJ';
+//     console.log(msg);
 // }
-// console.log(msg)
-// console.log(i)
+// console.log(msg);
+// console.log(i);
 
 // ==============
 // LEXICAL SCOPE
 // ==============
 
 function bankRobbery() {
-    const heroes = ['Spiderman', 'Wolverine', 'Black Panther', 'Batwoman']
+    const heroes = ['Spiderman', 'Wolverine', 'Black Panther', 'Batwoman'];
     function cryForHelp() {
-        let color = 'purple';
         function inner() {
             for (let hero of heroes) {
-                console.log(`PLEASE HELP US, ${hero.toUpperCase()}`)
+                console.log(`PLEASE HELP US, ${hero.toUpperCase()}`);
             }
         }
         inner();
     }
     cryForHelp();
 }
+
+bankRobbery();
