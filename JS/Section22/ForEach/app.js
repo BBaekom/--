11a00ba5e19575493076f41574876f6418cc022e
Pig <min208406@naver.com>
2023-08-02 00:17:25 +0900

    forEach Complete

--- JS/Section22/ForEach/app.js
+++ JS/Section22/ForEach/app.js
@@ -30,6 +30,7 @@ const movies = [
 ]
 
 movies.forEach(function (movie) {
-    console.log(`${movie.title} - ${movie.score}/100`)
-})
+    console.log(`${movie.title} - ${movie.score}/100`);
+});
+
 
