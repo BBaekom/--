f4417d3fbfb5da9f1adcd74b8679fedd76b3afb9
Pig <min208406@naver.com>
2023-08-01 17:29:03 +0900

    Method 끝

--- JS/Section21/Methods/app.js
+++ JS/Section21/Methods/app.js
@@ -8,14 +8,5 @@ const myMath = {
     }
 }
 
-const cat = {
-    name: 'Blue Steele',
-    color: 'grey',
-    breed: 'scottish fold',
-    meow() {
-        console.log("THIS IS:", this)
-        console.log(`${this.name} says MEOWWWW`);
-    }
-}
-
-const meow2 = cat.meow;
+console.log(myMath.PI);
+console.log(myMath["cube"](4));
