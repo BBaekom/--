aa2d0373bb2ae062a00668b8656acd073fdeade9
Pig <min208406@naver.com>
2023-08-02 01:38:55 +0900

    Filter End

--- JS/Section22/Filter/app.js
+++ JS/Section22/Filter/app.js
@@ -1,9 +1,10 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 numbers.filter(n => {
-    return n < 10
-})
+    return n < 10;
+});
 
+// console.log(newNums);
 
 const movies = [
     {
@@ -53,13 +54,23 @@ const movies = [
     }
 ]
 
+// const goodMovies = movies.filter(movie => {
+//     return movie.score > 80;
+// });
 
+const goodMovies = movies.filter(m => m.score > 80);
+// const goodTitles = goodMovies.map(m => m.title);
 
-const badMovies = movies.filter(m => m.score < 70)
+const goodTitles = movies
+    .filter(m => m.score > 80)
+    .map(m => m.title);
 
-const recentMovies = movies.filter(m => m.year > 2000)
+const badMovies = movies.filter(m => m.score < 70);
 
-// const goodMovies = movies.filter(m => m.score > 80)
-// const goodTitles = goodMovies.map(m => m.title)
+console.log(goodMovies);
+console.log(badMovies);
 
-movies.filter(m => m.score > 80).map(m => m.title);
+const recentMovies = movies.filter(m => m.year > 2000);
+console.log(recentMovies);
+
+console.log(goodTitles);
