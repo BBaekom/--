cc81f396f558ba558136b7282002bbe7b364a623
Pig <min208406@naver.com>
2023-08-02 18:31:47 +0900

    Rest End

--- JS/Section23/Rest/app.js
+++ JS/Section23/Rest/app.js
@@ -2,14 +2,16 @@
 //     return arguments.reduce((total, el) => total + el)
 // }
 
-// COLLECT THE "REST" IN NUMS:
-function sum(...nums) {
-    return nums.reduce((total, el) => total + el)
-}
+// function sum(...nums) {
+//     return nums.reduce((total, el) => total + el)
+// }
 
+// console.log(sum(34, 65, 77))
 
 function raceResults(gold, silver, ...everyoneElse) {
     console.log(`GOLD MEDAL GOES TO: ${gold}`)
     console.log(`SILVER MEDAL GOES TO: ${silver}`)
-    console.log(`AND THANKS TO EVERYONE ELSE: ${everyoneElse}`)
+    console.log(`AND THANKS TO EVERYONE ELSE : ${everyoneElse}`)
 }
+
+console.log(raceResults('Tammy', 'Todd', 'Tina', 'Trevor', 'Travis'));
