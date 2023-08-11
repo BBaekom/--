e4c16198f656020f9c9194871351ec6816b8f054
Pig <min208406@naver.com>
2023-08-11 10:11:40 +0900

    Axios End

--- JS/Section28/Axios/app.js
+++ JS/Section28/Axios/app.js
@@ -1,11 +1,10 @@
-// axios
-//   .get("https://swapi.dev/api/people/1/")
-//   .then((res) => {
+// axios.get("https://swapi.dev/api/people/1/")
+//   .then(res => {
 //     console.log("RESPONSE: ", res);
 //   })
-//   .catch((e) => {
+//   .catch(e => {
 //     console.log("ERROR! ", e);
-//   });
+//   })
 
 const getStarWarsPerson = async (id) => {
   try {
