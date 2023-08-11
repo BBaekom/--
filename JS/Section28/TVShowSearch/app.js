7d7669a5414248b387e6535762b667758dcadb22
Pig <min208406@naver.com>
2023-08-11 11:09:59 +0900

    Section28 End

--- JS/Section28/TVShowSearch/app.js
+++ JS/Section28/TVShowSearch/app.js
@@ -1,9 +1,9 @@
-const form = document.querySelector('#searchForm');
+const form = document.querySelector('#searchForm')
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
-    const searchTerm = form.elements.query.value;
+    const searchTerm = form.elements.query.value
     const config = { params: { q: searchTerm } }
-    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config)
     makeImages(res.data)
     form.elements.query.value = '';
 })
@@ -13,7 +13,7 @@ const makeImages = (shows) => {
         if (result.show.image) {
             const img = document.createElement('IMG');
             img.src = result.show.image.medium;
-            document.body.append(img)
+            document.body.append(img);
         }
     }
 }
