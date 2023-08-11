2d4e4aea7799713466d8a2fb05bae5045361bdd8
Pig <min208406@naver.com>
2023-08-11 09:49:59 +0900

    XMLHttp End

--- JS/Section28/XMLHTTP/app.js
+++ JS/Section28/XMLHTTP/app.js
@@ -4,14 +4,15 @@ const req = new XMLHttpRequest();
 
 req.onload = function () {
   console.log("IT LOADED!!");
-  const data = JSON.parse(this.responseText);
-  console.log(data.name, data.height);
-};
+  const data = JSON.parse(this.responseText)
+  console.log(data.name, data.height, data.hair_color);
+}
 
 req.onerror = function () {
-  console.log("ERROR!!!!");
+  console.log("ERROR!!!");
   console.log(this);
-};
+}
 
-req.open("GET", "https://swapi.dev/api/people/1/");
+req.open("GET", "https://swapi.dev/api/people/1/")
 req.send();
+
