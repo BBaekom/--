1d65cd636a10d66efff4e704b6c6a50f5b402b94
Pig <min208406@naver.com>
2023-08-20 21:42:56 +0900

    todolist

--- WEBPROGRAMMING/Todolist.js
+++ WEBPROGRAMMING/Todolist.js
@@ -1,4 +1,9 @@
-let click = document.querySelector(".addNew");
-click.addEventListener("click", function () {
+const clicks = document.querySelectorAll(".addNew");
 
-});
+for (let click of clicks) {
+    click.addEventListener("click", function () {
+        const toDoList = document.createElement('div');
+
+    })
+}
+
